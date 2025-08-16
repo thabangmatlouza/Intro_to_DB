17c6c07a2ce8b0c495a1e6996b0b6b1bff8199bc
Thabang <patolebekoholdings@gmail.com>
2025-08-16 18:20:23 +0200

    Fix task_4.sql to include TABLE_NAME = 'Books' and push all tasks

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,12 @@
 USE alx_book_store;
 
-SELECT 
-    COLUMN_NAME AS 'Column',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books';
+SELECT COLUMN_NAME AS Field,
+       COLUMN_TYPE AS Type,
+       IS_NULLABLE AS Null,
+       COLUMN_KEY AS `Key`,
+       COLUMN_DEFAULT AS `Default`,
+       EXTRA AS Extra
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
 
